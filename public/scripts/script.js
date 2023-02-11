daca6213515b34075b746872dab1e2d98aadfc39
Robin <robin.django@gmail.com>
2023-02-11 18:31:05 +0100

    removed old design

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,23 +1,53 @@
-var timeHour = new Date().getHours()
-var timeMinute = new Date().getMinutes()
-var ampm = "AM";
-const clockLabel = document.getElementById('clock-time');
+import * as THREE from '../../node_modules/three/build/three.module.js';
+import { GLTFLoader } from '../../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 
-console.log(timeHour + ":" + timeMinute);
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const loader = new THREE.GLTFLoader();
 
-function updateClock() {
-  timeHour = new Date().getHours()
-  timeMinute = new Date().getMinutes()
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+// renderer.outputEncoding = THREE.sRGBEncoding;
+document.body.appendChild( renderer.domElement );
 
-  if (timeHour > 12) {
-    ampm = "PM";
-    timeHour = timeHour - 12;
-  } else {
-    ampm = "AM";
-  }
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-   clockLabel.innerHTML = timeHour + ":" + timeMinute + " " + ampm;
+camera.position.z = 5;
+
+loader.load(
+	// resource URL
+	'../models/room/scene.gltf',
+	// called when the resource is loaded
+	function ( gltf ) {
+
+		scene.add( gltf.scene );
+
+		gltf.animations; // Array<THREE.AnimationClip>
+		gltf.scene; // THREE.Group
+		gltf.scenes; // Array<THREE.Group>
+		gltf.cameras; // Array<THREE.Camera>
+		gltf.asset; // Object
+
+	},
+	// called while loading is progressing
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+);
+
+function animate() {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
 }
-
-updateClock();
-setInterval(updateClock, 60*1000);
+animate();
